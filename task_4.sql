6b80625af9d5714b35d5991cc75e851969580bfc
Mike Kagwi <mikekagwi440@gmail.com>
2025-12-14 13:07:25 +0300

    Show full description of books table using INFORMATION_SCHEMA

---
+++ task_4.sql
@@ -0,0 +1,14 @@
+-- Select the database
+USE alx_book_store;
+
+-- Show full description of the 'books' table
+SELECT 
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'books';
